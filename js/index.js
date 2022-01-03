38a4e28090eef79ae964659484dc7b1aad4b3384
Vibeke-dev <vibeke.granhoej.joergensen@lego.com>
2022-01-03 21:03:17 +0100

    bonus1 added

--- js/index.js
+++ js/index.js
@@ -48,3 +48,29 @@ else if (hacker2.localeCompare(hacker1)===-1){
 else{
     console.log("What?! You both have the same name?");
 }
+
+//Bonus1
+const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a sem nisl. Nam in rutrum ligula, at sagittis ex. Ut non leo libero. Donec enim eros, blandit non congue eget, porta vel mauris. Sed bibendum, enim in consectetur tincidunt, elit augue varius tellus, et tristique odio orci vel enim. Curabitur commodo leo condimentum mi blandit imperdiet. Maecenas a fringilla erat. Fusce ut vulputate est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque cursus lorem sed sodales luctus. Praesent quis faucibus ante. Suspendisse lacinia varius tempor.
+ 
+Nullam condimentum id libero vel bibendum. Ut malesuada sapien vitae leo lobortis dictum. Fusce ut erat at purus venenatis blandit mattis eu velit. Aenean tristique eros sem, ut finibus velit consequat nec. Morbi pharetra fringilla pretium. In dictum lectus eget diam dignissim porta. Nulla ut eleifend erat. Donec magna metus, posuere vel varius at, porttitor vel massa. Nunc at fermentum nisl. Proin faucibus, diam at viverra semper, leo nisl fringilla ex, mattis interdum turpis lorem sit amet leo. Duis mi ipsum, fringilla sit amet massa eget, pretium elementum velit. Morbi tristique, turpis et ultricies lacinia, velit felis molestie diam, eu placerat quam sem sed sem. Donec non magna nec lacus porta ultricies ac id augue. Nam sed nunc et sapien feugiat cursus.
+ 
+Donec tincidunt mauris at massa finibus consectetur. Nunc cursus et felis a congue. Aliquam purus ante, facilisis in justo id, gravida finibus tortor. Vivamus id tortor a libero venenatis tempor. Mauris et tristique nibh, et vehicula ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eu lacus eget purus finibus eleifend. Integer ornare ornare risus eget sodales. Maecenas auctor faucibus molestie. Vivamus eu ex enim. Pellentesque sit amet tempor mauris, quis pretium nulla. Duis porta vulputate lectus sed bibendum. Nam vitae neque vitae lectus vulputate sollicitudin. Vivamus vitae pulvinar leo. Mauris congue venenatis leo at luctus.`;
+
+let numberOfWord = 0;
+let numberOfEt = 0;
+
+for (let i=0; i<=loremIpsum.length; i++){
+    if(loremIpsum[i] === ' '){
+        numberOfWord++;
+    }
+}
+
+let j=0;
+
+while (loremIpsum.indexOf(' et ', j)>=0){
+    j=loremIpsum.indexOf(' et ', j)+1;
+    numberOfEt++;
+}
+      
+
+console.log(`number of words: ${numberOfWord} and number of et: ${numberOfEt}`);
